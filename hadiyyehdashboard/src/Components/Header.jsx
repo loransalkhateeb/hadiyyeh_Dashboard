6efaeca4e392eec52de2f1c50ce4203007364229
dania altarifi <dania.a.altarifi@gmail.com>
2024-10-23 16:57:44 +0300

    header

--- hadiyyehdashboard/src/Components/Header.jsx
+++ hadiyyehdashboard/src/Components/Header.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import { BiLogOut } from "react-icons/bi";
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <div className="d-flex">
+      {/* Sidebar */}
+      <div className="sidebar bg-light" style={{ width: "250px" }}>
+        <p>Sidebar</p>
+      </div>
+
+      <div className="flex-grow-1">
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+          <div className="container-fluid">
+            <img
+              src={require("../Images/Logo.avif")}
+              alt="logo"
+              height={"80"}
+            />
+            <a className="navbar-brand" href="#">
+              Dashboard
+            </a>
+            <div
+              className="navbar-collapse justify-content-end"
+              id="navbarSupportedContent"
+            >
+              <BiLogOut size="1.4rem" />
+            </div>
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
