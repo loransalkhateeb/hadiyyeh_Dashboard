99a61a0ada1d623b056585a984358d27a3562ea4
dania altarifi <dania.a.altarifi@gmail.com>
2024-10-24 14:01:12 +0300

    new header

--- hadiyyehdashboard/src/App.js
+++ hadiyyehdashboard/src/App.js
@@ -1,13 +1,27 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./Components/Header";
+import Products from './Pages/Products/Products';
+import './App.css'
+import Users from "./Pages/Users/Users";
+import Orders from "./Pages/Orders/Orders";
+import Brands from "./Pages/Brands/Brands";
+import DiscountCode from "./Pages/DiscountCode/DiscountCode";
+
 function App() {
   return (
     <div className="App">
       <Router>
-        <Header />
-        <Routes></Routes>
+        <Routes>
+          <Route path="/" element={<Header />}>
+            <Route path="products" element={<Products />} />
+            <Route path="users" element={<Users />} />
+            <Route path="orders" element={<Orders />} />
+            <Route path="brands" element={<Brands />} />
+            <Route path="codes" element={<DiscountCode />} />
+            {/* Add other routes here if needed */}
+          </Route>
+        </Routes>
       </Router>
     </div>
   );
---
+++ hadiyyehdashboard/src/Pages/Products/Products.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Products() {
+  return (
+    <div>
+      <h2>Products</h2>
+      <p>This is the Products page content.</p>
+    </div>
+  );
+}
+
+export default Products;
