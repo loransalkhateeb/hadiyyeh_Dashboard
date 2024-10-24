a6b986264f0cb116706c723cd4816288a588f4d3
loransalkhateeb <lwrnsalkhtyb9@gmail.com>
2024-10-24 09:41:31 +0300

    Adding the pages of the dashboard

--- hadiyyehdashboard/src/App.js
+++ hadiyyehdashboard/src/App.js
@@ -1,14 +1,25 @@
-
-import './App.css';
-import SideBar from './Components/SideBar'
-
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import SideBar from './Components/SideBar'; // Adjust the path as necessary
+import Home from './Pages/Home';       // Your Home component
+import Orders from './Pages/Orders/orders';   // Your Orders component
+import Products from './Pages/Products/Products'; // Your Products component
+import Users from './Pages/Users/users';     // Your Users component
+import Codes from './Pages/Codes/codes';     // Your Codes component
 
 function App() {
-  return (
-    <div className="App">
-       <SideBar />
-    </div>
-  );
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/orders" element={<Orders />} />
+                <Route path="/products" element={<Products />} />
+                <Route path="/users" element={<Users />} />
+                <Route path="/codes" element={<Codes />} />
+            </Routes>
+        <SideBar/>
+        </>
+    );
 }
 
 export default App;
--- hadiyyehdashboard/src/Components/SideBar.jsx
+++ hadiyyehdashboard/src/Components/SideBar.jsx
@@ -1,15 +1,63 @@
-// src/Sidebar.js
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-
-function SideBar(){
-    return(
+function SideBar() {
+    return (
         <>
-        {
-            
-        }
+        <div className="container-fluid">
+            <div className="row flex-nowrap">
+                <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0" style={{ backgroundColor: '#f8f9fa' }}>
+                    <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-dark min-vh-100">
+                        <Link to="/" className="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+                            <img src='https://hadiyyeh.com/cdn/shop/files/Hadiyyeh_Logo_Transparent_New.png?v=1711312656&width=70' alt="Logo" />
+                        </Link>
+                        <ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
+                            <li className="nav-item">
+                                <Link to="/" className="nav-link align-middle px-0">
+                                    <i className="fs-4 bi-house"></i> <span className="ms-1 d-none d-sm-inline">Home</span>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/orders" className="nav-link px-0 align-middle">
+                                    <i className="fs-4 bi-table"></i> <span className="ms-1 d-none d-sm-inline">Orders</span>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/users" className="nav-link px-0 align-middle">
+                                    <i className="fs-4 bi-table"></i> <span className="ms-1 d-none d-sm-inline">Users</span>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/products" className="nav-link px-0 align-middle">
+                                    <i className="fs-4 bi-table"></i> <span className="ms-1 d-none d-sm-inline">Products</span>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/codes" className="nav-link px-0 align-middle">
+                                    <i className="fs-4 bi-people"></i> <span className="ms-1 d-none d-sm-inline">Codes</span>
+                                </Link>
+                            </li>
+                        </ul>
+                        <hr />
+                        <div className="dropdown pb-4">
+                            <a href="#" className="d-flex align-items-center text-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+                                <img src="https://github.com/mdo.png" alt="user" width="30" height="30" className="rounded-circle" />
+                                <span className="d-none d-sm-inline mx-1">User</span>
+                            </a>
+                            <ul className="dropdown-menu dropdown-menu-light text-small shadow">
+                                <li><a className="dropdown-item" href="#">New project...</a></li>
+                                <li><a className="dropdown-item" href="#">Settings</a></li>
+                                <li><a className="dropdown-item" href="#">Profile</a></li>
+                                <li><hr className="dropdown-divider" /></li>
+                                <li><a className="dropdown-item" href="#">Sign out</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         </>
-    )
+    );
 }
 
-export default SideBar
+export default SideBar;
